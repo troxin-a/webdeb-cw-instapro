3305b71c22262f1bd355db536a458b5e55b252df
Anton Pashinov <troxin1988@gmail.com>
2024-09-10 11:02:20 +0700

    Реализовал страницу с постами конкретного юзера - 1 час

--- api.js
+++ api.js
@@ -23,7 +23,6 @@ export function getPosts({ token }) {
     });
 }
 
-// https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
 export function registerUser({ login, password, name, imageUrl }) {
   return fetch(baseHost + "/api/user", {
     method: "POST",
@@ -88,4 +87,24 @@ export function addPost({ token, description, imageUrl }) {
     }
     return response.json();
   });
+}
+
+// Получает посты конкретного пользователя
+export function getPostsUser({ token, userId }) {
+  return fetch(postsHost + `/user-posts/${userId}`, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
+      
+      return response.json();
+    })
+    .then((data) => {
+      return data.posts;
+    });
 }
